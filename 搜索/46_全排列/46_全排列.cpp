10ec2dfbec17c452a497d2c7c858924544afa66e
yiouejv <yiouejv@126.com>
2021-02-08 05:53:57 +0000

    [搜索][回溯]全排列46,47

--- 搜索/46_全排列/46_全排列.cpp
+++ 搜索/46_全排列/46_全排列.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-    	if (nums.empty()) return {};
-    	vector<vector<int>> ans;
-    	for (int i = 0; i < nums.size() - 1; ++i) {
-			backtracking(nums, ans, i);
-    	}
-    	return ans;
+        if (nums.empty()) return {};
+        vector<vector<int>> ans;
+        vector<int> path;
+        vector<bool> used(nums.size(), false);
+        dfs(nums, ans, path, used, 0);
+        return ans;
     }
 
-    void backtracking(vector<int> &nums, vector<vector<int>> &ans, int level) {
-    	if (level == nums.size() - 1) {
-
-    		ans.push_back(nums);
-    		return;
-    	}
-
-		ans.push_back(nums);
-    	for (int i = level+1; i < nums.size(); ++i) {
-    		swap(nums[level], nums[i]);
-    		backtracking(nums, ans, level+1);
-    		swap(nums[level], nums[i]);
-    	}
+    void dfs(vector<int>& nums, vector<vector<int>>& ans, vector<int>& path, vector<bool>& used, int depth)
+    {
+        if (nums.size() == depth)
+        {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (!used[i])
+            {
+                used[i] = true;
+                path.push_back(nums[i]);
+                dfs(nums, ans, path, used, depth+1);
+                used[i] = false;
+                path.pop_back();
+            }
+        }
     }
 }; 
